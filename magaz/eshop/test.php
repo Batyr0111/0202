c9353cca2b164e7f7a01fbdbe12b56d14d83537a
Batyr0111 <63508792+Batyr0111@users.noreply.github.com>
2020-04-11 21:54:14 +0600

    02

---
+++ magaz/eshop/test.php
@@ -0,0 +1,10 @@
+<?php
+	$name = 'name';
+		$email = '123';
+		$phone = '52492';
+		$adress = 'hi';
+		$filename = $basket['orderid'];
+		$str = "$name;$email;$phone;$adress;\n";
+	printf($str);
+	printf($str);
+?>
